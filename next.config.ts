91efbf90c4694c11c052bda8f1abbf428e2be5e4
Nguyen Thanh Son <42330924+sonnguyendata@users.noreply.github.com>
2025-09-17 16:58:53 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 // next.config.ts
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  eslint: { ignoreDuringBuilds: true },     // don't fail Vercel builds on ESLint
-  typescript: { ignoreBuildErrors: true },  // don't fail on TS type errors
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
