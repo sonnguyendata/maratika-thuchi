79748e5dafa967ea8a4f804f0dfe5bee1ac80ae4
Nguyen Thanh Son <42330924+sonnguyendata@users.noreply.github.com>
2025-09-18 12:37:12 +0700

    Add explicit pdf-parse type declaration

--- types/pdf-parse-lib.d.ts
+++ types/pdf-parse-lib.d.ts
@@ -1,4 +1,5 @@
-import type { PdfParseFn } from '../../src/lib/pdfParse';
+import type { PdfParseFn } from '../src/lib/pdfParse';
+
 declare module 'pdf-parse/lib/pdf-parse.js' {
   const pdfParse: PdfParseFn;
   export default pdfParse;
