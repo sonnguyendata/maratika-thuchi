2fd1f98075d16fdfe26dbcacab24af5ad6488c44
Nguyen Thanh Son <42330924+sonnguyendata@users.noreply.github.com>
2025-09-18 10:09:00 +0700

    chore: move pdf-parse types

--- types/pdf-parse/index.d.ts
+++ types/pdf-parse/index.d.ts
@@ -1,5 +1,4 @@
-import type { PdfParseFn } from '../src/lib/pdfParse';
-
+import type { PdfParseFn } from '../../src/lib/pdfParse';
 declare module 'pdf-parse/lib/pdf-parse.js' {
   const pdfParse: PdfParseFn;
   export default pdfParse;
